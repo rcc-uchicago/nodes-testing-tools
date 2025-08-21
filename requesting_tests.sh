a0975746242e0a7375436228b8030f4e1583f9fe
Trung Nguyen <ndtrung@uchicago.edu>
2025-08-21 08:31:50 -0500

    fixed indentation

--- requesting_tests.sh
+++ requesting_tests.sh
@@ -34,23 +34,23 @@ while true; do
     timestamp=`date`
     reservation_exist=`scontrol show res $reservation | grep ReservationName | sed 's/ReservationName=//g'`
     echo "Reservation: $reservation_exist"
-    if [ -n "$reservation_exist" ]; then 
-    
-				echo $timestamp
+    if [ -n "$reservation_exist" ]; then
+ 
+        echo $timestamp
         scontrol show res $reservation > res_info.txt
         nodelist=`grep Nodes res_info.txt | awk '{print $1}'| sed 's/Nodes=//g'`
-				num_nodes=`grep NodeCnt res_info.txt | awk '{print $2}'| sed 's/NodeCnt=//g'`
-				max_cores=`grep CoreCnt res_info.txt | awk '{print $3}'| sed 's/CoreCnt=//g'`
+        num_nodes=`grep NodeCnt res_info.txt | awk '{print $2}'| sed 's/NodeCnt=//g'`
+        max_cores=`grep CoreCnt res_info.txt | awk '{print $3}'| sed 's/CoreCnt=//g'`
 
         scontrol show node $nodelist > node_info.txt
         real_mem=`grep RealMemory node_info.txt | awk '{print $1}'| sed 's/RealMemory=//g'`
-				max_ppn=`grep CPUTot node_info.txt | awk '{print $2}'| sed 's/CPUTot=//g'`
-				has_gpu=`grep "gpu:" node_info.txt`
+        max_ppn=`grep CPUTot node_info.txt | awk '{print $2}'| sed 's/CPUTot=//g'`
+        has_gpu=`grep "gpu:" node_info.txt`
         gpu_nodes=0
         gres=""
         if [ -n "$has_gpu" ]; then
             gpu_nodes=1
-            gres=`grep "Gres=" node_info.txt | awk '{print $1}'| sed 's/Gres=//g'` 
+            gres=`grep "Gres=" node_info.txt | awk '{print $1}'| sed 's/Gres=//g'`
         fi
 
         output="output-$nodelist.txt"
@@ -84,26 +84,26 @@ while true; do
         fi
 
         if [ $enforce -eq 1 ]
-        then 
-      		  running_test=1
+        then
+            running_test=1
         fi
 
 
-				# Submitting job to compute node
-				# job_script.sh builds
+        # Submitting job to compute nodes
+
         if [ $running_test -eq 1 ]
         then
 
             echo "Testing $nodelist .."
-            
+
             job_script="queue-$nodelist.txt"
             if [ $gpu_nodes -eq 1 ]
             then
-    						cp queue-gpu-nodes.txt $job_script
+                cp queue-gpu-nodes.txt $job_script
                 echo "GPU: $gres"
                 sed -i "s/--gres=gpu:4/--gres=$gres/g" $job_script
             else
-   						  cp queue-cpu-nodes.txt $job_script
+                cp queue-cpu-nodes.txt $job_script
             fi
 
             # modify the job script
@@ -112,11 +112,12 @@ while true; do
 
             echo "Submitting job script $job_script.."
 
-            sbatch --nodelist=$nodelist --ntasks-per-node=$max_ppn $job_script $output
+            #sbatch --nodelist=$nodelist --ntasks-per-node=$max_ppn $job_script $output
+            sbatch $job_script $output
             echo "Check output $output for testing results"
 
         fi
-				
+
     fi
 
     sleep $delay  # Check every 300 seconds
